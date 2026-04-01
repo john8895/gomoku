ae3de3df38eabcc4018795a4b26550cbb35becc3
john8895 <john8895@users.noreply.github.com>
2026-04-01 23:00:30 +0800

    add: AI opponent with scoring heuristic (attack/defense eval)

--- gomoku-logic.js
+++ gomoku-logic.js
@@ -3,6 +3,13 @@
 
 var BOARD_SIZE = 15;
 
+var DIRECTIONS = [
+  [0, 1],   // horizontal
+  [1, 0],   // vertical
+  [1, 1],   // diagonal backslash
+  [1, -1]   // diagonal slash
+];
+
 function createBoard() {
   var i, j, board = [];
   for (i = 0; i < BOARD_SIZE; i++) {
@@ -15,16 +22,10 @@ function createBoard() {
 }
 
 function checkWin(board, r, c, player) {
-  var directions = [
-    [0, 1],   // horizontal
-    [1, 0],   // vertical
-    [1, 1],   // diagonal backslash
-    [1, -1]   // diagonal slash
-  ];
   var i, dr, dc, count, nr, nc;
-  for (i = 0; i < directions.length; i++) {
-    dr = directions[i][0];
-    dc = directions[i][1];
+  for (i = 0; i < DIRECTIONS.length; i++) {
+    dr = DIRECTIONS[i][0];
+    dc = DIRECTIONS[i][1];
     count = 1;
     nr = r + dr; nc = c + dc;
     while (nr >= 0 && nr < BOARD_SIZE && nc >= 0 && nc < BOARD_SIZE && board[nr][nc] === player) {
@@ -40,6 +41,92 @@ function checkWin(board, r, c, player) {
   }
   return false;
 }
+
+// ---------- AI scoring ----------
+
+// Score a line of `count` consecutive stones with `openEnds` open endpoints
+function lineScore(count, openEnds) {
+  if (openEnds === 0) return 0;
+  if (count >= 5) return 100000;
+  if (count === 4) return openEnds === 2 ? 50000 : 10000;
+  if (count === 3) return openEnds === 2 ? 5000 : 500;
+  if (count === 2) return openEnds === 2 ? 100 : 10;
+  if (count === 1) return openEnds === 2 ? 5 : 1;
+  return 0;
+}
+
+// Evaluate the score gained by placing `player` at (r, c) on board
+function evalCell(board, r, c, player) {
+  var i, dr, dc, count, openEnds, nr, nc, score = 0;
+  for (i = 0; i < DIRECTIONS.length; i++) {
+    dr = DIRECTIONS[i][0];
+    dc = DIRECTIONS[i][1];
+    count = 1;
+    openEnds = 0;
+
+    // forward
+    nr = r + dr; nc = c + dc;
+    while (nr >= 0 && nr < BOARD_SIZE && nc >= 0 && nc < BOARD_SIZE && board[nr][nc] === player) {
+      count++;
+      nr += dr; nc += dc;
+    }
+    if (nr >= 0 && nr < BOARD_SIZE && nc >= 0 && nc < BOARD_SIZE && board[nr][nc] === 0) {
+      openEnds++;
+    }
+
+    // backward
+    nr = r - dr; nc = c - dc;
+    while (nr >= 0 && nr < BOARD_SIZE && nc >= 0 && nc < BOARD_SIZE && board[nr][nc] === player) {
+      count++;
+      nr -= dr; nc -= dc;
+    }
+    if (nr >= 0 && nr < BOARD_SIZE && nc >= 0 && nc < BOARD_SIZE && board[nr][nc] === 0) {
+      openEnds++;
+    }
+
+    score += lineScore(count, openEnds);
+  }
+  return score;
+}
+
+// Pick best move for `aiPlayer` against `humanPlayer`
+// Returns {r, c} or null if board is full
+function getAiMove(board, aiPlayer, humanPlayer) {
+  var r, c, attackScore, defenseScore, totalScore;
+  var bestScore = -1;
+  var bestMoves = [];
+
+  for (r = 0; r < BOARD_SIZE; r++) {
+    for (c = 0; c < BOARD_SIZE; c++) {
+      if (board[r][c] !== 0) continue;
+
+      // Temporarily place stone to evaluate
+      board[r][c] = aiPlayer;
+      attackScore = evalCell(board, r, c, aiPlayer);
+      board[r][c] = 0;
+
+      board[r][c] = humanPlayer;
+      defenseScore = evalCell(board, r, c, humanPlayer);
+      board[r][c] = 0;
+
+      // Slightly favour attack; heavily penalise letting opponent win
+      totalScore = attackScore * 1.1 + defenseScore;
+
+      if (totalScore > bestScore) {
+        bestScore = totalScore;
+        bestMoves = [{ r: r, c: c }];
+      } else if (totalScore === bestScore) {
+        bestMoves.push({ r: r, c: c });
+      }
+    }
+  }
+
+  if (bestMoves.length === 0) return null;
+  // Break ties randomly for variety
+  return bestMoves[Math.floor(Math.random() * bestMoves.length)];
+}
+
+// ---------- game state ----------
 
 // Place a stone; returns 'win', 'draw', or 'continue'
 function placeStone(state, r, c) {
@@ -87,6 +174,9 @@ module.exports = {
   createBoard: createBoard,
   createState: createState,
   checkWin: checkWin,
+  evalCell: evalCell,
+  lineScore: lineScore,
+  getAiMove: getAiMove,
   placeStone: placeStone,
   undoMove: undoMove
 };
